cd4f40bce4b8d3d13302dd19b7ebf875161c264f
JackSmith555 <136043677@qq.com>
2016-06-30 11:59:48 +0800

    don't joke me

---
+++ orangewater.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bitset>
+#include <vector>
+#include <cstdlib>
+#include <cstdio>
+//#include "loginout.h"
+//#include "core.h"
+//#include "loader.h"
+
+using namespace std;
+
+int main(int agrc , char** agr)
+{
+	
+	cout<<"hello , world!"<<endl;
+	return 0;
+}
